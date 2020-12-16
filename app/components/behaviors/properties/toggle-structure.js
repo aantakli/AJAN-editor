78c3013d99f7a9e4aa24e3ac91b4a9951c252497
unknown <andre.antakli@dfki.de>
2020-12-16 14:05:19 +0100

    removed logic to remove toggle RDF and implemented way of checking undefined toggle info

--- app/components/behaviors/properties/toggle-structure.js
+++ app/components/behaviors/properties/toggle-structure.js
@@ -20,8 +20,9 @@
  */
 import Component from "@ember/component";
 import rdfGraph from "ajan-editor/helpers/RDFServices/RDF-graph";
-import { ND, RDF } from "ajan-editor/helpers/RDFServices/vocabulary";
 import rdfTree from "ajan-editor/helpers/RDFServices/manager/tree";
+import rdfFact from "ajan-editor/helpers/RDFServices/RDF-factory";
+import { ND, RDF } from "ajan-editor/helpers/RDFServices/vocabulary";
 import generator from "ajan-editor/helpers/RDFServices/manager/generator";
 
 let that = undefined;
@@ -33,12 +34,10 @@ export default Component.extend({
   oldFirst: undefined,
   oldLast: undefined,
 
-  didInsertElement() {
+  didReceiveAttrs() {
     this._super(...arguments);
     that = this;
-    let quad = rdfGraph.findQuad(this.get("uri"), ND.toggle);
-    this.set("toggleValue", quad.object.value === ND.First);
-    this.set("oldValue", quad.object.value === ND.First);
+    setToggleValue();
   },
 
   didUpdate() {
@@ -47,66 +46,79 @@ export default Component.extend({
       this.set("oldValue", this.get("toggleValue"));
       if (this.get("toggleValue")) {
         quad.object.value = ND.First;
-        let deleted = deleteOldToggleRDF(this.get("toggle.last"));
-        console.log(deleted);
-        this.set("oldLast", deleted);
-        if (this.get("oldFirst") === undefined) {
-          generator.generateStructure(this.get("toggle.first"), this.get("uri"), false);
-        } else {
-          this.get("oldFirst").forEach(quad => {
-            rdfGraph.add(quad);
-          })
-        }
+        deleteOldToggleRDF(this.get("toggle.last"));
+        generator.generateStructure(this.get("toggle.first"), this.get("uri"), false);
       } else {
         quad.object.value = ND.Last;
-        let deleted = deleteOldToggleRDF(this.get("toggle.first"));
-        console.log(deleted);
-        this.set("oldFirst", deleted);
-        if (this.get("oldLast") === undefined) {
-          generator.generateStructure(this.get("toggle.last"), this.get("uri"), false);
-        } else {
-          this.get("oldLast").forEach(quad => {
-            rdfGraph.add(quad);
-          })
-        }
+        deleteOldToggleRDF(this.get("toggle.first"));
+        generator.generateStructure(this.get("toggle.last"), this.get("uri"), false);
       }
     }
   },
 });
 
+function setToggleValue() {
+  let quad = rdfGraph.findQuad(that.get("uri"), ND.toggle);
+  if (quad === undefined) {
+    let toggle = that.get("toggle");
+    let quads = rdfGraph.getAllQuads(that.get("uri"));
+    quads.forEach(quad => {
+      let predicate = quad.predicate.value;
+      if (checkArrays(predicate, toggle.last)) {
+        rdfGraph.add(rdfFact.quad(that.get("uri"), ND.toggle, ND.Last));
+        that.set("toggleValue", false);
+        that.set("oldValue", false);
+      } else {
+        rdfGraph.add(rdfFact.quad(that.get("uri"), ND.toggle, ND.First));
+        that.set("toggleValue", true);
+        that.set("oldValue", true);
+      }
+    });
+  } else {
+    that.set("toggleValue", quad.object.value === ND.First);
+    that.set("oldValue", quad.object.value === ND.First);
+  }
+}
+
+function checkArrays(predicate, toggleItem) {
+  if (checkMapsTo(predicate, toggleItem.parameters)) return true;
+  else if (checkMapsTo(predicate, toggleItem.parameterSets)) return true;
+  else if (checkMapsTo(predicate, toggleItem.list)) return true;
+  else return false;
+}
+
+function checkMapsTo(predicate, array) {
+  array.forEach(item => {
+    if (item.mapping === predicate) return true;
+  })
+  return false;
+}
+
 function deleteOldToggleRDF(obj) {
-  let removed = [];
   if (obj.list.length > 0) {
     obj.list.forEach(item => {
-      removed = removed.concat(removeAllRDF(that.get("uri"), item.mapping));
+      removeAllRDF(that.get("uri"), item.mapping);
     })
   } else if (obj.parameters.length > 0) {
     obj.parameters.forEach(item => {
-      removed = removed.concat(removeAllRDF(that.get("uri"), item.mapping));
+      removeAllRDF(that.get("uri"), item.mapping);
     })
   } else if (obj.parameterSets.length > 0) {
     obj.parameterSets.forEach(item => {
-      removed = removed.concat(removeAllRDF(that.get("uri"), item.mapping));
+      removeAllRDF(that.get("uri"), item.mapping);
     })
   }
-  console.log(removed);
-  return removed;
 }
 
 function removeAllRDF(URI, predicate) {
-  let removed = [];
   let quad = rdfGraph.findQuad(URI, predicate);
   if (quad != undefined) {
     let nodes = [];
     rdfTree.visitNode(quad.object.value, nodes);
     nodes.forEach(uri => {
-      let quads = rdfGraph.getAllQuads(uri);
-      quads.forEach(quad => {
-        removed.push(quad)
-      });
+      console.log(uri);
       rdfGraph.removeAllRelated(uri);
     });
     rdfGraph.remove(quad);
   }
-  return removed;
 }

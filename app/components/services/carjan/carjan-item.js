7646091734830fe9b75da62c86b58d567a0d73b9
Leonard Neis <leonard@neis-it.de>
2024-09-24 20:47:34 +0200

    Loading maps from .json file and colorize grid

--- app/components/services/carjan/carjan-item.js
+++ app/components/services/carjan/carjan-item.js
@@ -16,33 +16,47 @@ export default Component.extend({
     this.setupCanvas();
     this.setupPanningAndZoom();
   },
-
   setupCanvas() {
     const canvas = this.element.querySelector("#gridCanvas");
     const ctx = canvas.getContext("2d");
     const gridSize = 12;
     const cellSize = 50;
 
+    // Farbwerte aus den CSS-Variablen holen
+    const rootStyles = getComputedStyle(document.documentElement);
+    const colorPrimary = rootStyles.getPropertyValue('--color-primary').trim();
+    const colorPrimary1 = rootStyles.getPropertyValue('--color-primary-1').trim();
+    const colorPrimary2 = rootStyles.getPropertyValue('--color-primary-2').trim();
+    const colorPrimary3 = rootStyles.getPropertyValue('--color-primary-3').trim();
+    const colorPrimary4 = rootStyles.getPropertyValue('--color-primary-4').trim();
+    const colorPrimary5 = rootStyles.getPropertyValue('--color-primary-5').trim();
+    const colorPrimary6 = rootStyles.getPropertyValue('--color-primary-6').trim();
+
+    const colors = [colorPrimary, colorPrimary1, colorPrimary2, colorPrimary3, colorPrimary4, colorPrimary5, colorPrimary6];
+
     const drawGrid = () => {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.strokeStyle = "#000";
 
-      for (let i = 0; i <= gridSize; i++) {
-        ctx.beginPath();
-        ctx.moveTo(i * cellSize, 0);
-        ctx.lineTo(i * cellSize, canvas.height);
-        ctx.stroke();
+      for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+          // Verwende abwechselnd Farben aus dem Farbarray für das Schachbrettmuster
+          const colorIndex = (i + j) % colors.length;
+          const cellColor = colors[colorIndex];
 
-        ctx.beginPath();
-        ctx.moveTo(0, i * cellSize);
-        ctx.lineTo(canvas.width, i * cellSize);
-        ctx.stroke();
+          // Fülle die Zellen mit der entsprechenden Farbe
+          ctx.fillStyle = cellColor;
+          ctx.fillRect(i * cellSize, j * cellSize, cellSize, cellSize);
+
+          // Zeichne die Rahmen der Zellen
+          ctx.strokeStyle = "#000";
+          ctx.strokeRect(i * cellSize, j * cellSize, cellSize, cellSize);
+        }
       }
     };
 
     drawGrid();
-    this.centerGrid();
-  },
+  }
+,
 
   centerGrid() {
     const canvas = this.element.querySelector("#gridCanvas");

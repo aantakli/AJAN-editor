423306c70e4f1c04d3b8bc0c2777abb4ba0f93df
Leonard Neis <leonard@neis-it.de>
2024-10-04 18:38:24 +0200

    add icons drag to sidebar

--- app/components/services/carjan/carjan-dragdrop.js
+++ app/components/services/carjan/carjan-dragdrop.js
@@ -3,7 +3,24 @@ import Component from "@ember/component";
 export default Component.extend({
   actions: {
     onDragStart(event) {
-      event.dataTransfer.setData("text", event.target.id);
+      const entityType = event.target.id.replace("-icon", "");
+
+      event.dataTransfer.setData("text", entityType);
+
+      const iconMap = {
+        pedestrian: "#pedestrian-icon",
+        vehicle: "#car-icon",
+        autonomous: "#autonomous-icon",
+        obstacle: "#obstacle-icon",
+        map: "#map-icon",
+      };
+
+      const dragIconSelector = iconMap[event.target.id] || "#map-icon";
+      const dragIcon = document.querySelector(dragIconSelector);
+
+      if (dragIcon) {
+        event.dataTransfer.setDragImage(dragIcon, 10, 10);
+      }
     },
   },
 });
